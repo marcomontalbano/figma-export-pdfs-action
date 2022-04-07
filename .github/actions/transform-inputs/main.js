73013cd1a851cb3f18f4b09d350bda6789e42397
Marco Montalbano <marcomontalbano.work@gmail.com>
2022-04-07 22:51:59 +0200

    Update rules for the 'ids' output

--- .github/actions/transform-inputs/main.js
+++ .github/actions/transform-inputs/main.js
@@ -7,15 +7,14 @@ const fileKey = core.getInput('fileKey', { required: true });
 const page = JSON.parse(core.getInput('page', { required: true }) || '{}');
 const selection = JSON.parse(core.getInput('selection', { required: false }) || '[]');
 
-function removeUndefined(value) {
-  return value !== undefined
-}
+const selectionIds = selection.map(node => node.id)
+const pageIds = page.id ? [page.id] : []
+const ids = selectionIds.length > 0 ? selectionIds : pageIds
+
+core.startGroup('Outputs')
+core.info(`fileKey: ${ fileKey }`)
+core.info(`ids: ${ JSON.stringify(ids) }`)
+core.endGroup()
 
 core.setOutput('fileKey', fileKey);
-core.setOutput(
-  'ids',
-  selection
-    .map(node => node.id)
-    .concat(page.id)
-    .filter(removeUndefined)
-);
+core.setOutput('ids', ids);
